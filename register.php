64716cf6647df87a8b65b7ff31bcc843063756bb
mehdi <mehdimehdipour21@gmail.com>
2024-04-20 17:05:54 +0330

    updated login and register page for the front-end.

--- register.php
+++ register.php
@@ -4,19 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/login.css">
+
     <title>Document</title>
 </head>
 <body>
 <div class="container">
-    <h1>ورود</h1>
+    <h1>عضویت</h1>
     <input style="margin-bottom:70px;" type="text" placeholder="Email">
     <input type="text" placeholder="Password">
     <div class="signup-link">
-        <p> حساب کاربری ندارید؟ <a href="./register.php"> ثبت نام کنید </a></p>
+        <p> حساب کاربری دارید؟ <a href="./login.php"> وارد شوید </a></p>
     </div>
-<!--    <button>ورود</button>-->
-    <input type="submit" name="send" value="ورود" class="btn btn-primary">
-
+<!--        <button>ثبت نام</button>-->
+    <input type="submit" name="send" value="ثبت نام" class="btn btn-primary">
 </div>
+
+
 </body>
 </html>
