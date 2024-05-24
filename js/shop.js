923673cfdcc62325ba8ac7d81084273f82ddd80f
llaryall <arya.xll17llx@gmail.com>
2024-05-25 00:59:31 +0330

    changed shop page UI

--- js/shop.js
+++ js/shop.js
@@ -1,41 +1,45 @@
-const images = document.querySelectorAll('.container a');
-const container2 = document.getElementById('container2');
-const container = document.getElementById ('container');
-const closebtn = document.getElementById('close');
-const body = document.getElementById('body');
-const fullArrow= document.getElementById('fullar');
-const maximum = document.getElementById("max-page");
-const previmg = document.getElementById("prev-img");
+const previewtab = document.getElementById("preview");
+const shop = document.getElementById("shop");
+const imge = document.querySelectorAll("#shop img");
+const previtem = document.getElementById('one')
+const closeprev =document.getElementById('closeprev')
+const linkPage = document.getElementById('linkPage')
+
+let prevopen = false;
+
+function toggleprev() {
+  const shop = document.getElementById("shop");
+  const previewtab = document.getElementById("preview");
+
+  if (prevopen) {
+    // Close the preview tab
+    shop.style.marginLeft = "12rem";
+    previewtab.style.width = "0px";
+    previtem.style.opacity= 0
+    closeprev.style.opacity=0
+    linkPage.style.opacity=0
+
+    prevopen = false;
+  } else {
+    // Open the preview tab
+    shop.style.marginLeft = "0px";
+    previewtab.style.width = "24rem";
+    previtem.style.opacity= 1
+    closeprev.style.opacity=1
+    linkPage.style.opacity=1
 
 
+    prevopen = true;
+  }
+}
+function closeprevtab(){
+    shop.style.marginLeft = "12rem";
+    previewtab.style.width = "0px";
+    previtem.style.opacity= 0
 
-  
-
-images.forEach(image => {
-  image.addEventListener('click', () => {
-    container.style.transform = 'translateX(-5.5%)';
-    container.classList.add('gap')
-    event.preventDefault();
-    container2.style.display = 'flex'; // Show container2
-    container2.style.width = '370px'; // Fade in 
-    maximum.style.display = "block";
-    closebtn.style.display = "block";
-    previmg.style.display = "block";
-
-
-  });
+    prevopen = false;
+}
+imge.forEach((image) => {
+  image.addEventListener("click", toggleprev);
 });
-
-closebtn.addEventListener('click' , () => {
-    container2.style.width = '0px';
-    container.style.transform = 'translateX(0%)';
-    container.classList.remove('gap');
-    container.style.width = '100vw';
-    closebtn.style.display = 'none';
-    
-})
-
-maximum.addEventListener('click' , () => {
-  container2.style.width = '100vw';
-  container2.classList.add('fullprev');
-} )
+closeprev.addEventListener('click' , closeprevtab)
