203d5bbb3a43cf5cf5da4fc45df6581ee15644bd
Zahra Karami <zahroo1680@gmail.com>
2024-06-03 23:22:24 +0330

    add anew category to slider

--- functions/f-slider.php
+++ functions/f-slider.php
@@ -1,1 +1,16 @@
 <?php
+include_once 'connect.php';
+function add_slider($info,$img){
+    $title=$info['title'];
+    $url=$info['url'];
+    if (isset($info['status'])) {
+        $status = $info['status'];
+    } else {
+        $status = 'off';
+    }
+
+    $pdo = connect_db();
+    $query=$pdo->prepare("INSERT INTO slider_tbl (title,url,img,status) 
+    VALUES ('$title','$url','$img','$status')");
+    $query->execute();
+}
