7925ab20000bc0e0e4920112b6bff7e47eccc09b
Zahra Karami <zahroo1680@gmail.com>
2024-06-11 23:09:06 +0330

    make a edit for slider list

--- functions/f-slider.php
+++ functions/f-slider.php
@@ -21,3 +21,23 @@ function list_slider(){
     $res=$query->fetchAll(PDO::FETCH_OBJ);
     return $res;
 }
+function edit_slider($id){
+    $pdo = connect_db();
+    $query=$pdo->prepare("SELECT * FROM slider_tbl WHERE id='$id'");
+    $query->execute();
+    $res=$query->fetch(PDO::FETCH_OBJ);
+    return $res;
+}
+function update_slider($id,$info,$img){
+    $title=$info['title'];
+    $url=$info['url'];
+    if (isset($info['status'])) {
+        $status = $info['status'];
+    } else {
+        $status = 'off';
+    }
+
+    $pdo = connect_db();
+    $query=$pdo->prepare("UPDATE slider_tbl SET title='$title',url='$url',img='$img',status='$status' WHERE id = '$id'");
+    $query->execute();
+}
